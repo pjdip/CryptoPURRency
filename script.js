1b25e0f29f6d25235316aa0f82d9dbd077c17438
pjdip <pjdip1@gmail.com>
2020-11-19 22:12:47 -0500

    added clear history button, top10btn functionality, working on jQuery dynamic api tomfoolery

--- script.js
+++ script.js
@@ -26,6 +26,47 @@ function coinDuplicate(coin) {
     return duplicate;
 }
 
+// clear the history when clicked
+$("#clearHistory").on("click", function(event) {
+    event.preventDefault();
+    coinHistory = [];
+    storeCoins();
+    renderHistory();
+});
+
+function renderTop10() {
+    $("#top10").empty();
+    var top10URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+    $.ajax({url: top10URL, method: "GET"}).then(function(response) {
+        for (var i = 0; i < 10; i++) {
+            var newCoin = $("<li>").text(response[i].name).attr("id", response[i].name).attr("class", "top10btn");
+            $("#top10").apppend(newCoin);
+        }
+    });
+
+    $(".top10btn").on("click", function(event) {
+        event.preventDefault();
+
+        // Grabbing the coin name
+        var topCoin = $(this).attr("id");
+
+        // Move the coin to the top of the history
+        if (coinDuplicate(topCoin) === true) {
+            var indX = coinHistory.indexOf(topCoin);
+            coinHistory.splice(indX, 1);
+        }
+        coinHistory.unshift(topCoin);
+
+        renderCoinData(topCoin);
+        storeCoins();
+        renderHistory();
+    });
+}
+
+function renderCoinData(coinVar) {
+
+}
+
 timeID: 24h, 7d, 14d, 30d, 60d, 200d, 1y
 currencyID: usd, eur, gbp, btc, eth, etc...
 
@@ -49,46 +90,55 @@ description:
     $("#projectDescription").text(response.description);
     response.description
 
-market cap:
-
-function renderTop10() {
-
-    $("#top10").empty();
-    var top10URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-    $.ajax({url: top10URL, method: "GET"}).then(function(response) {
-        for (var i = 1; i < 11; i++) {
-            var newCoin = $("<li>").text(response[i].name).attr("id", response[i].name);
-            $("#top10").apppend(newCoin);
-        }
-    })
-}
-
 
 // some logic to pull the top ten
     market_cap_rank
 
+// offer user choice of currency?
 market data:
     current price:
+        $("#currentPrice").text(market_data.current_price.usd);
+        
         market_data.current_price.(currencyID)
+    
     all time high:
+        $("#ATH").text(market_data.ath.usd);
+        $("#ATHdate").text(market_data.ath.ath_date);
+
         market_data.ath.(currencyID)
         market_data.ath_date
+
     all time low:
+        $("#ATL").text(market_data.atl.usd);
+
         market_data.atl.(currencyID)
+        
     marketcap:
+        $("#marketCap").text(market_data.market_cap.usd);
+    
         market_data.market_cap.currencyID
+    
     24h volume:
+        $("#tradingVolume").text(market_data.total_volume.usd);
+
         market_data.total_volume.currencyID
+    
     percent change:
+        $("#percentChangeIn24h").text(market_data.price_change_percentage_24h);
+        $("#percentChangeIn30d").text(market_data.price_change_percentage_30d);
+        $("#percentChangeIn200d").text(market_data.price_change_percentage_200d);
+        $("#percentChangeIn1y").text(market_data.price_change_percentage_1y);
+
         market_data.price_change_percentage_(timeID) (i think these are all in usd)
 
         market_data.price_change_24h_in_currency.(currencyID)
-
         market_data.price_change_percentage_(timeID)_in_currency.(currencyID)
 
     supply:
-        market_data.total_supply
-        market_data.max_suppy
+        $("#maxSupply").text(market_data.max_supply);
+        $("#circulatingSupply").text(market_data.circulating_supply);
+
+        market_data.max_supply
         market_data.circulating_supply
 
 developer data:
